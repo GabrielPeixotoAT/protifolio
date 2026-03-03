7ae9727c46b8f11d743becf722a89720d78ab436
Gabriel Peixoto <gabr.art.design@gmail.com>
2026-03-02 21:17:51 -0300

    fix: ajustes pós revisão

--- src/app/core/sections/hero-component/hero-component.ts
+++ src/app/core/sections/hero-component/hero-component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { NgStyle } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-hero-component',
-  imports: [],
+  imports: [NgStyle],
   templateUrl: './hero-component.html',
   styleUrl: './hero-component.scss',
 })
 export class HeroComponent {
+    blurValue: number = 0;
+    opacityValue: number = 1;
 
+    @HostListener('window:scroll', [])
+    onWindowScroll(): void {
+        const scrollPosition = window.scrollY || document.documentElement.scrollTop;
+    
+        let calcBlur = scrollPosition / 50;
+
+        let calcOpc = scrollPosition / 70;
+        
+        this.opacityValue = 1 - (Math.min(calcOpc, 9) / 10);
+        this.blurValue = Math.min(calcBlur, 15);
+    }
+
+    @HostListener('window:load', [])
+    onWindowLoad(): void {
+        const scrollPosition = window.scrollY || document.documentElement.scrollTop;
+    
+        let calcBlur = scrollPosition / 50;
+
+        let calcOpc = scrollPosition / 70;
+        
+        this.opacityValue = 1 - (Math.min(calcOpc, 9) / 10);
+        this.blurValue = Math.min(calcBlur, 15);
+    }
 }
